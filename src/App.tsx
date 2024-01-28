c6800c1044d3fa418d731b9b7b7dd8e1c37f6a1c
Abel Yoshuara <abelyoshuara@gmail.com>
2024-01-28 15:24:29 +0800

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -10,18 +10,13 @@ export default function App() {
     useState<boolean>(false);
 
   function handleUpdateDigit(data: string) {
-    // First Digit
     if (!waitingSecondDigit) {
-      console.log("first digit");
       setFirstDigit(firstDigit + data);
       setResult(firstDigit + data);
     } else {
-      console.log("second digit");
       setSecondDigit(secondDigit + data);
       setResult(secondDigit + data);
     }
-
-    console.log("first " + firstDigit, "second: " + secondDigit);
   }
 
   function handleOperator(data: string): void {
@@ -51,103 +46,101 @@ export default function App() {
   }
 
   return (
-    <>
-      <div className="container my-16">
-        <h1 className="text-slate-700 text-4xl font-bold text-center mt-20 mb-8">
-          Caculator App
-        </h1>
+    <div className="container my-16">
+      <h1 className="text-slate-700 text-4xl font-bold text-center mt-20 mb-8">
+        Caculator App
+      </h1>
 
-        <div className="max-w-xl mx-auto flex flex-col gap-y-3 border shadow-sm p-4 rounded-lg">
-          <div className="bg-slate-800 w-full p-10 rounded-lg text-slate-100 text-end text-[1.3rem] font-semibold">
-            <span id="displayNumber">{result}</span>
-          </div>
-          <div className="flex gap-3">
-            <button
-              className="btn-primary"
-              onClick={() => handleUpdateDigit("7")}
-            >
-              7
-            </button>
-            <button
-              className="btn-primary"
-              onClick={() => handleUpdateDigit("8")}
-            >
-              8
-            </button>
-            <button
-              className="btn-primary"
-              onClick={() => handleUpdateDigit("9")}
-            >
-              9
-            </button>
-            <button className="btn-primary negative">+/-</button>
-          </div>
-          <div className="flex gap-3">
-            <button
-              className="btn-primary"
-              onClick={() => handleUpdateDigit("4")}
-            >
-              4
-            </button>
-            <button
-              className="btn-primary"
-              onClick={() => handleUpdateDigit("5")}
-            >
-              5
-            </button>
-            <button
-              className="btn-primary"
-              onClick={() => handleUpdateDigit("6")}
-            >
-              6
-            </button>
-            <button
-              className="btn-primary operator"
-              onClick={() => handleOperator("-")}
-            >
-              -
-            </button>
-          </div>
-          <div className="flex gap-3">
-            <button
-              className="btn-primary"
-              onClick={() => handleUpdateDigit("1")}
-            >
-              1
-            </button>
-            <button
-              className="btn-primary"
-              onClick={() => handleUpdateDigit("2")}
-            >
-              2
-            </button>
-            <button
-              className="btn-primary"
-              onClick={() => handleUpdateDigit("3")}
-            >
-              3
-            </button>
-            <button
-              className="btn-primary operator"
-              onClick={() => handleOperator("+")}
-            >
-              +
-            </button>
-          </div>
-          <div className="flex gap-3">
-            <button className="btn-primary">AC</button>
-            <button
-              className="btn-primary basis-[50%]"
-              onClick={() => handleUpdateDigit("0")}
-            >
-              0
-            </button>
-            <button className="btn-primary operator" onClick={handleCalculate}>
-              =
-            </button>
-          </div>
+      <div className="max-w-xl mx-auto flex flex-col gap-y-3 border shadow-sm p-4 rounded-lg">
+        <div className="bg-slate-800 w-full p-10 rounded-lg text-slate-100 text-end text-[1.3rem] font-semibold">
+          <span id="displayNumber">{result}</span>
+        </div>
+        <div className="flex gap-3">
+          <button
+            className="btn-primary"
+            onClick={() => handleUpdateDigit("7")}
+          >
+            7
+          </button>
+          <button
+            className="btn-primary"
+            onClick={() => handleUpdateDigit("8")}
+          >
+            8
+          </button>
+          <button
+            className="btn-primary"
+            onClick={() => handleUpdateDigit("9")}
+          >
+            9
+          </button>
+          <button className="btn-primary negative">+/-</button>
+        </div>
+        <div className="flex gap-3">
+          <button
+            className="btn-primary"
+            onClick={() => handleUpdateDigit("4")}
+          >
+            4
+          </button>
+          <button
+            className="btn-primary"
+            onClick={() => handleUpdateDigit("5")}
+          >
+            5
+          </button>
+          <button
+            className="btn-primary"
+            onClick={() => handleUpdateDigit("6")}
+          >
+            6
+          </button>
+          <button
+            className="btn-primary operator"
+            onClick={() => handleOperator("-")}
+          >
+            -
+          </button>
+        </div>
+        <div className="flex gap-3">
+          <button
+            className="btn-primary"
+            onClick={() => handleUpdateDigit("1")}
+          >
+            1
+          </button>
+          <button
+            className="btn-primary"
+            onClick={() => handleUpdateDigit("2")}
+          >
+            2
+          </button>
+          <button
+            className="btn-primary"
+            onClick={() => handleUpdateDigit("3")}
+          >
+            3
+          </button>
+          <button
+            className="btn-primary operator"
+            onClick={() => handleOperator("+")}
+          >
+            +
+          </button>
+        </div>
+        <div className="flex gap-3">
+          <button className="btn-primary">AC</button>
+          <button
+            className="btn-primary basis-[50%]"
+            onClick={() => handleUpdateDigit("0")}
+          >
+            0
+          </button>
+          <button className="btn-primary operator" onClick={handleCalculate}>
+            =
+          </button>
         </div>
       </div>
-    </>
+    </div>
   );
 }
